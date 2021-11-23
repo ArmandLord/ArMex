fcea0f29b3ee897bba295ca6aa8bd66d278030b1
Fernanda <bihurriet.fer@gmail.com>
2021-11-23 14:51:11 -0300

    test endpoints

---
+++ server/src/test/createOnePlayer.test.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+const {Player,conn} = require('../db');
+
+
+describe('test endpoint createOnePlayer',()=>{
+    beforeEach(async ()=>{
+        await Object.values(conn.models).map(function(model) {
+            return model.destroy({ truncate: { cascade: true } });
+        }); 
+    });
+
+    afterEach(async ()=>{
+        await Object.values(conn.models).map(function(model) {
+            return model.destroy({ truncate: { cascade: true } });
+        });
+    });
+
+
+    it('createOnePlayer debe poder crear un player y guardarlo en la base de datos', async()  => {
+        // Sends GET Request to /test endpoint
+        const res = await request.post('/player/onePlayer').send({nickname:"guido", status:"oro", ranking:2, avatar:"https://res.cloudinary.com/dy9tey0yi/image/upload/v1637177226/Futbol%20players/cristiano-ronaldo_zm5u9x.png"})
+        const players = await Player.findOne({where:{nickname:"guido"}});
+        //creo un player y despues lo busca en la base de datos y espera que el unico player existente sea el creado
+        
+        expect(players.dataValues.nickname).toEqual("guido");
+    });
+});
---
+++ server/src/test/updatePlayer.test.js
@@ -0,0 +1,39 @@
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+const {Player,conn} = require('../db');
+
+
+
+describe('test endpoint updatePlayer',()=>{
+    beforeEach(async ()=>{
+        await Object.values(conn.models).map(function(model) {
+            return model.destroy({ truncate: { cascade: true } });
+        }); 
+    });
+
+    afterEach(async ()=>{
+        await Object.values(conn.models).map(function(model) {
+            return model.destroy({ truncate: { cascade: true } });
+        });
+    });
+
+
+    it('updatePLayer debe retornar un player actualizado', async()  => {
+        // Sends GET Request to /test endpoint
+
+        await Player.create({
+            nickname: "icardi",
+            status: "oro",
+            ranking : 2,
+            avatar : "https://res.cloudinary.com/dy9tey0yi/image/upload/v1637177226/Futbol%20players/cristiano-ronaldo_zm5u9x.png",
+        });
+        const players = await Player.findOne({where:{nickname:"icardi"}})
+        const res = await request.put('/player').send({id:players.dataValues.id,nickname:"messi",status:"plata",ranking:2,avatar:"https://res.cloudinary.com/dy9tey0yi/image/upload/v1637177226/Futbol%20players/cristiano-ronaldo_zm5u9x.png"})
+        const playerByPk = await Player.findOne({where:{nickname:"messi"}})
+        //creo un player y despues lo busca en la base de datos y espera que el unico player existente sea el creado
+        
+        expect(playerByPk.dataValues.nickname).toEqual("messi");
+        expect(playerByPk.dataValues.status).toEqual("plata");
+    });
+});
