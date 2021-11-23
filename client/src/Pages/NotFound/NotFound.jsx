f27e626e47ce235e0cdced2faecdec4a8b95c6ef
Gabriel Sanchez <54195784+GabSanWebDev@users.noreply.github.com>
2021-11-23 17:31:44 -0300

    feat(page 404)

--- client/src/Pages/NotFound/NotFound.jsx
+++ client/src/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,13 @@
-import { NotFoundContainer, JustifyNotFound } from './NotFound.styled'
+import { NotFoundContainer, JustifyNotFound, BtnBack, NavLinks } from './NotFound.styled'
 
 const NotFound = () => {
     return (
         <NotFoundContainer>
             <JustifyNotFound>
-                No exite papuuu
+                <img src="https://res.cloudinary.com/dy9tey0yi/image/upload/v1637698362/Futbol%20players/Oops_page_not_found_d5mn9b.png" width="100%" styles={{margin: "0 auto"}} />
+                <NavLinks to='/'>
+            <BtnBack>Return</BtnBack>
+            </NavLinks>
             </JustifyNotFound>
         </NotFoundContainer>
     )
--- client/src/Pages/NotFound/NotFound.styled.js
+++ client/src/Pages/NotFound/NotFound.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Container } from '../../GlobalStyles/GlobalStyles'
+import { Link } from 'react-router-dom'
 
 export const NotFoundContainer = styled.div`
     background: rgb(156,18,56);
@@ -13,3 +14,36 @@ export const JustifyNotFound = styled(Container)`
     flex-wrap: wrap;
     height: calc(100vh - 60px); //este es mientras no supere el height del contenido
 `
+
+export const BtnBack = styled.button`
+    margin: 0 auto;
+    width: 10rem;
+    height: 3rem;
+    font-size:1.5rem;
+    background-color: #000000;
+    color: #e0e0e0;
+    border: none;
+    border-radius: 1rem;
+
+    &:hover{
+        color: #ffffff;;
+        cursor: pointer;
+    }
+`
+export const NavLinks = styled(Link)`
+    color: #e0e0e0;
+    text-decoration: none;
+    margin: 0 auto;
+
+    &:hover{
+        color: #ffffff;
+    }
+
+    @media screen and (max-width: 960px) {
+        text-align: center;
+        text-transform: uppercase;
+        padding: 2rem;
+        width: 100%;
+        display: table;
+    }
+ `
